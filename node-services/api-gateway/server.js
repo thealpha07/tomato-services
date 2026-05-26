788dd0257d21c5d4e22b1d7884085544ec621383
Adarsh Sadanand Shetty <adarshsadanand7@gmail.com>
2026-05-26 09:26:18 +0530

    CORS update

--- node-services/api-gateway/server.js
+++ node-services/api-gateway/server.js
@@ -11,7 +11,7 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS
 
 app.use(cors({
   origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
+    if (!origin || allowedOrigins.includes("*") || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
